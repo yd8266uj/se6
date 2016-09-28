a1daa82be5cf3acea85ec08a1ec18745aae3988b
yd8266uj <yd8266uj@metrostate.edu>
2016-09-27 22:38:27 -0500

    updated sql connection info in php

--- db_fns.php
+++ db_fns.php
@@ -1,7 +1,7 @@
 <?php
 
 function db_connect() {
-   $result = new mysqli('localhost', 'book_sc', 'password', 'book_sc');
+   $result = new mysqli('localhost', 'ics499fa160124', 'password', 'book_sc');
    if (!$result) {
       return false;
    }

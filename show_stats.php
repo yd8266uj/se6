37a0d10fcd73fc96258a5bad7ca28f7bc105c1b6
yd8266uj <yd8266uj@metrostate.edu>
2016-10-02 23:50:24 -0500

    Update show_stats.php

--- show_stats.php
+++ show_stats.php
@@ -4,11 +4,12 @@
   session_start();
 
   do_html_header("Database Statistics");
+
   $result =get_statistics();
-printf("Categories: %d<br>\n",$result["categories_count"]);
-printf("Total Number of Books: %d<br>\n",$result["books_count"]);
-printf("Total Cost of the Books: \$%.2f<br>\n",$result["books_total_price"]);
-printf("Average Cost of the Book: \$%.2f<br>\n",$result["books_average_price"]);
+  printf("Categories: %d<br>\n",$result["categories_count"]);
+  printf("Total Number of Books: %d<br>\n",$result["books_count"]);
+  printf("Total Cost of the Books: \$%.2f<br>\n",$result["books_total_price"]);
+  printf("Average Cost of the Book: \$%.2f<br>\n",$result["books_average_price"]);
  
   do_html_footer();
 ?>
